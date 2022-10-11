6a3a370b8611543e243df8d6b4f44b393de4697a
Aakriti0108 <111855055+Aakriti0108@users.noreply.github.com>
2022-10-11 16:07:51 +0530

    Task5

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,11 @@
 const user = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
+function generateAccessToken(id)
+{
+    return jwt.sign({userId:id},'aakriti')
+}
 
 
 function isstringinvalid(string)
@@ -45,5 +50,42 @@ exports.signup = async (req,res)=>{
     {
         console.log(err)
         res.status(500).json({err})
+    }
+}
+
+exports.login= async(req,res)=>{
+    try{
+        const email = req.body.email;
+        const password = req.body.password;
+       
+        // console.log(email)
+        if(isstringinvalid(email) || isstringinvalid(password))
+        {
+        return res.status(400).json({err:"bad parameters . something is missing"})
+        }
+    
+         const User = await  user.findAll({where:{email:email}})
+       console.log(User)
+       if(User)
+       {
+        bcrypt.compare(password,User[0].password,(err,result)=>{
+            if(err)
+            {
+                res.status(500).json({success:false , message:"something went wrong"})
+            }
+            if(result == true)
+            {
+                res.status(200).json({success:true , message:"user is successfully logged" , token: generateAccessToken(User[0].id)})
+            }
+            else
+            {
+                return res.status(400).json({success:false , message:"Password is incorrect"})
+            }
+           })
+       }
+     
+    }
+    catch(err){
+        res.status(404).json({message:"user doesn't exist",err})
     }
 }
